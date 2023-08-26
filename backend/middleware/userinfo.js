55ff3acd2462d786729ae47906a4141720fc0814
Chetan <agarwalchaitan1065@gmail.com>
2023-08-26 21:10:06 +0530

    added few endpoints

---
+++ backend/middleware/userinfo.js
@@ -0,0 +1,17 @@
+const jwt = require('jsonwebtoken');
+const JWT_SECRET = 'HelloGreatUser$100'
+
+const userinfo = (req,res,next)=>{
+    const token = req.header('auth-token')
+    if(!token){
+        res.status(401).send({error : "Please authenticate with a valid auth token"})
+    }
+    try {
+        const userdata =jwt.verify(token,JWT_SECRET);
+        req.user = userdata.user;
+        next();
+    } catch (error) {
+        res.status(401).send({error : "Please authenticate with a valid auth token"})
+    }
+}
+module.exports = userinfo;
--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -1,26 +1,87 @@
 const express = require('express')
 const User = require('../models/User')
+const bcrypt = require('bcryptjs');
 const router = express.Router()
 const { body, validationResult } = require('express-validator');
+const jwt = require('jsonwebtoken')
+const JWT_SECRET = 'HelloGreatUser$100'
+var userinfo = require('../middleware/userinfo')
 
-router.post('/createuser',[
-    body('name','name length min 4').isLength({min:4}),
-    body('email','Insert a valid Email').isEmail(),
-    body('password','password length min 4').isLength({min:5}),
-],async (req,res)=>{
+//Endpoint 1 : Signup for a user
+router.post('/createuser', [
+    body('name', 'name length min 4').isLength({ min: 4 }),
+    body('email', 'Insert a valid Email').isEmail(),
+    body('password', 'password length min 4').isLength({ min: 5 }),
+], async (req, res) => {
     const errors = validationResult(req);
-  if (!errors.isEmpty()) {
-    return res.status(400).json({ errors: errors.array() });
-  }
-  let user = await User.findOne({email:req.body.email})
-  if(user){
-    return res.status(400).json({error : "user already exists!"})
-  }
-   user = await User.create({
-    name : req.body.name,
-    email : req.body.email,
-    password : req.body.password
-  })
-  res.json(user)
+    if (!errors.isEmpty()) {
+        return res.status(400).json({ errors: errors.array() });
+    }
+    let user = await User.findOne({ email: req.body.email })
+    if (user) {
+        return res.status(400).json({ error: "user already exists!" })
+    }
+    const salt = await bcrypt.genSalt(10);
+    const secPass = await bcrypt.hash(req.body.password, salt);
+    user = await User.create({
+        name: req.body.name,
+        email: req.body.email,
+        password: secPass
+    })
+    const data = {
+        user: {
+            id: user.id
+        }
+    }
+    const authtoken = jwt.sign(data, JWT_SECRET)
+    res.json({ authtoken })
 })
+
+// Endpoint 2 : Login of User
+
+router.post('/login', [
+    body('email', 'Email is not valid').isEmail(),
+    body('password', 'Password is required').exists(),
+], async (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+        return res.status(400).json({ errors: errors.array() });
+    }
+    const { email, password } = req.body
+    try {
+        let user = await User.findOne({ email })
+        if (!user) {
+            return res.status(400).json({ wrong: "Please login with correct credentials !" })
+        }
+        const correct = await bcrypt.compare(password, user.password)
+        if (!correct) {
+            return res.status(400).json({ wrong: "Please login with correct credentials !" })
+        }
+        const data = {
+            user: {
+                id: user.id
+            }
+        }
+        const authtoken = jwt.sign(data, JWT_SECRET)
+        res.json({ authtoken })
+
+    } catch (error) {
+        res.status(404).send("Server did not respond !");
+    }
+})
+
+// Endpoint 3 : Fetching User Information 
+
+router.post('/info', userinfo , 
+    async (req, res) => {
+
+        try {
+            userID = req.user.id;
+            const user = await User.findById(userID).select("-password")
+            res.send(user)
+        } catch (error) {
+            res.status(404).send("Server did not respond !");
+        }
+    })
+
 module.exports = router
